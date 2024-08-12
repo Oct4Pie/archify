ea3df15020c704245171fde15710335d21c93f4b
Oct4pie <>
2024-08-12 01:15:46 -0700

    disk access checks

--- archify/archifyhelper/HelperTool.swift
+++ archify/archifyhelper/HelperTool.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 class HelperTool: NSObject, NSXPCListenerDelegate, HelperToolProtocol {
+    static let version = Version.current
+
     func duplicateApp(appDir: String, outputDir: String, withReply reply: @escaping (String?, String?) -> Void) {
         // Implementation for duplicateApp if needed
     }
@@ -74,14 +76,38 @@ class HelperTool: NSObject, NSXPCListenerDelegate, HelperToolProtocol {
     }
     
     func checkFullDiskAccess(withReply reply: @escaping (Bool) -> Void) {
-            NSLog("Checking for full disk access.")
-            let testPath = "/Library/Application Support/com.apple.TCC/TCC.db"
-            let hasAccess = FileManager.default.isReadableFile(atPath: testPath)
-            reply(hasAccess)
-            if hasAccess {
-                NSLog("Full disk access granted.")
+        NSLog("Checking for full disk access.")
+        
+        let paths = [
+            "~/Library/Safari/Bookmarks.plist",
+            "~/Library/Safari/CloudTabs.db",
+            "/Library/Application Support/com.apple.TCC/TCC.db",
+            "/Library/Preferences/com.apple.TimeMachine.plist"
+        ]
+        
+        var hasAccess = true
+        
+        for path in paths {
+            let expandedPath = (path as NSString).expandingTildeInPath
+            if FileManager.default.fileExists(atPath: expandedPath) {
+                if !FileManager.default.isReadableFile(atPath: expandedPath) {
+                    hasAccess = false
+                    NSLog("Cannot read file at path: \(expandedPath)")
+                    break
+                }
             } else {
-                NSLog("Full disk access not granted.")
+                NSLog("File does not exist at path: \(expandedPath)")
+                hasAccess = false
+                break
             }
         }
+        
+        reply(hasAccess)
+        
+        if hasAccess {
+            NSLog("Full disk access granted.")
+        } else {
+            NSLog("Full disk access not granted.")
+        }
+    }
 }
